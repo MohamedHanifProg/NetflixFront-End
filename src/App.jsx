1a6cf2dbe231d5822a371ab7faa71f62f6604287
MohamedHanifProg <signutre.31@gmail.com>
2025-04-08 15:59:56 +0300

    Initial clean React Vite setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// src/App.jsx
+import { Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/home" element={<Home />} />
+    </Routes>
+  );
+}
+
+export default App;
