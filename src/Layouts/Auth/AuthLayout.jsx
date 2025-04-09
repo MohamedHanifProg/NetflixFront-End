ce55a8db4f450c2c7486de1ebbe18ac6dc25fc19
MohamedHanifProg <signutre.31@gmail.com>
2025-04-09 18:26:50 +0300

    Background image fixed

--- src/Layouts/Auth/AuthLayout.jsx
+++ src/Layouts/Auth/AuthLayout.jsx
@@ -1,14 +1,17 @@
-// src/layouts/AuthLayout.jsx
+// src/Layouts/Auth/AuthLayout.jsx
 import React from 'react';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
-import './AuthLayout.css';  // We'll create this next
+import './AuthLayout.css'; // You'll create this next
 
 const AuthLayout = ({ children }) => {
   return (
-    <div className="auth-layout">
+    <div className="auth-page">
+      <div className="auth-overlay"></div>
       <Header />
-      <div className="auth-content">{children}</div>
+      <div className="auth-content">
+        {children}
+      </div>
       <Footer />
     </div>
   );
