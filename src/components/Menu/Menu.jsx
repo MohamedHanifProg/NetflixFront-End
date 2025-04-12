696680ddbbffc90abd62b216739ed5e9508b4eeb
MohamedHnef <mohamed.hnef1@gmail.com>
2025-04-12 18:48:11 +0300

    neflix logo takes return to home page

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -2,46 +2,35 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import "./Menu.css";
-
 function Menu() {
-    // useLocation helps us determine which path we are currently on
     const location = useLocation();
-
-    // Checks if the current path starts with the given string
     const isActive = (path) => location.pathname.startsWith(path);
-
     return (
         <nav className="menu-bar">
             <div className="menu-left">
-                <img
-                    src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png"
-                    alt="Netflix Logo"
-                    className="menu-logo"
-                />
-
-                {/* If your Home route is /home, check for /home instead of "/" */}
+               
+                <Link to="/home">
+                    <img
+                        src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png"
+                        alt="Netflix Logo"
+                        className="menu-logo"
+                    />
+                </Link>
                 <Link to="/home" className={isActive("/home") ? "active" : ""}>
                     Home
                 </Link>
-
-                {/* Use the same path (/tv-shows) for the Link and isActive */}
                 <Link to="/tv-shows" className={isActive("/tv-shows") ? "active" : ""}>
                     TV Shows
                 </Link>
-
                 <Link to="/movies" className={isActive("/movies") ? "active" : ""}>
                     Movies
                 </Link>
-
                 <Link to="/new-popular" className={isActive("/new-popular") ? "active" : ""}>
                     New & Popular
                 </Link>
-
                 <Link to="/mylist" className={isActive("/mylist") ? "active" : ""}>
                     My List
                 </Link>
-
-
                 <Link to="/browse" className={isActive("/browse") ? "active" : ""}>
                     Browse
                 </Link>
